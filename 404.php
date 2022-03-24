e40472999c0a17012b4698b8941353e083a79489
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-03-24 14:30:19 -0300

    404 Template & Search

---
+++ 404.php
@@ -0,0 +1,13 @@
+<!-- File for page not found 404 -->
+
+<?= get_header(); ?>
+
+<article class="content px-3 py-5 p-md-5" style="display: flex; flex-direction:column; align-items:center; gap: 1em;">
+    <h1>Page not found</h1>
+    <h4 for="">Search for a post</h4>
+    <!-- Search form -->
+    <?= get_search_form(); ?>
+</article>
+
+
+<?= get_footer(); ?>
--- search.php
+++ search.php
@@ -1,5 +1,18 @@
-<?php
+<!-- Display search results -->
 
-// Search results
+<?= get_header(); ?>
 
-?>
+<article class="content px-3 py-5 p-md-5">
+    <?php
+        if (have_posts()) {
+            while (have_posts()) {
+                the_post();
+                get_template_part('template-parts/content', 'archive');
+            }
+        }
+        
+        the_posts_pagination();
+    ?>
+</article>
+
+<?= get_footer(); ?>
