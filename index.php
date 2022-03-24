4e0874132ee3c4c7d0775a89871f579c8f0d66e0
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-03-23 21:28:17 -0300

    Pagination Links

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 
 <article class="content px-3 py-5 p-md-5">
     <?php
+        // Wordpress Loop
         if (have_posts()) {
             while (have_posts()) {
                 the_post();
@@ -12,6 +13,9 @@
             }
         }
     ?>
+
+    <!-- Pagination -->
+    <?= the_posts_pagination(); ?>
 </article>
 
 
